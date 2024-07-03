2034addfbd3581683c9deee7c96cfaddbf7c4c25
jason zheng <tso1158687@gmail.com>
2024-07-03 22:30:09 +0800

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // TODO: Enable CORS in development mode
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
