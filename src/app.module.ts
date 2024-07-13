0e1cbb65cc63ff7cef2ee5d9bf892421a31ead74
jason zheng <tso1158687@gmail.com>
2024-07-13 12:54:14 +0800

    add conversation module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { GeminiModule } from './gemini/gemini.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConversationModule } from './conversation/conversation.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot('mongodb://localhost:27017/question_list'),
     GeminiModule,
+    ConversationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
