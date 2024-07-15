ce6a09968256b6c3b709cb85c19ac445dbd27f08
jason zheng <tso1158687@gmail.com>
2024-07-15 17:18:31 +0800

    add websocket gateway failed

---
+++ src/gemini/gemini.gateway.ts
@@ -0,0 +1,35 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
+
+@WebSocketGateway({namespace: 'gemini'})
+export class GeminiGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('ChatGateway');
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, payload: string): void {
+    this.server.emit('message', payload);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+}
--- src/gemini/gemini.module.ts
+++ src/gemini/gemini.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { GeminiService } from './gemini.service';
 import { GeminiController } from './gemini.controller';
 import { ConversationModule } from 'src/conversation/conversation.module';
+import { GeminiGateway } from './gemini.gateway';
 
 @Module({
   imports: [ConversationModule],
-  providers: [GeminiService],
+  providers: [GeminiService, GeminiGateway],
   controllers: [GeminiController],
 })
 export class GeminiModule {}
